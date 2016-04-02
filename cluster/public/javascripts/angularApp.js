671e6f487c38a25e9baf88c361677369cb677749
John <student@students-imac-9.home>
2016-04-01 19:29:17 -0600

    done

---
+++ cluster/public/javascripts/angularApp.js
@@ -0,0 +1,16 @@
+angular.module('clusterApp', [])
+.controller('MainCtrl', [
+	'$scope', '$http',
+	function($scope, $http) {
+		$scope.cluster = [];
+		$scope.getPIDs = function () {
+			for(var i = 0; i < 100; i++) {
+				$http.get('/pid').success(function(data) {
+					console.log('get all');
+					console.log(data);
+					$scope.cluster.push(data);
+				});
+			}
+		};
+	}
+]);
---
+++ cluster/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', pid: process.pid });
+});
+
+router.get('/pid', function(req, res, next) {
+	console.log('pid:', process.pid);
+	res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+	res.header('Pragma', 'no-cache');
+	res.header('Expires', 0);
+	res.json({title: 'Cluster', pid: process.pid });
+});
+
+module.exports = router;
